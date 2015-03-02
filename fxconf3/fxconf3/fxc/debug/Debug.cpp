df44165ce649a518fe8fc486563f3b882552a49e
x000000 <o.o.n.joy@gmail.com>
2015-03-02 20:18:44 +0300

    Добавлена возможность добавления служебной информации к стеку вызовов
    Signed-off-by: x000000 <o.o.n.joy@gmail.com>

--- fxconf3/fxconf3/fxc/debug/Debug.cpp
+++ fxconf3/fxconf3/fxc/debug/Debug.cpp
@@ -5,11 +5,11 @@
 
 #if DEBUG
 
-	fxc::debug::SourcePoint::SourcePoint(const char *filename, int line) : filename(filename), line(line) {}
+	fxc::debug::SourcePoint::SourcePoint(const char *filename, int line, const std::string data) : filename(filename), line(line), data(data) {}
 
 	std::vector<fxc::debug::SourcePoint> fxc::debug::StackTrace::_callstack;
-	void fxc::debug::StackTrace::push(const char *filename, int line) {
-		_callstack.push_back(SourcePoint(filename, line));
+	void fxc::debug::StackTrace::push(const char *filename, int line, const std::string data) {
+		_callstack.push_back(SourcePoint(filename, line, data));
 	}
 	void fxc::debug::StackTrace::pop() {
 		_callstack.pop_back();
@@ -19,7 +19,12 @@
 		ss << "Stack trace (" << _callstack.size() << "):\r\n";
 
 		for (int i = 0, l = _callstack.size(); i < l; ++i) {
-			ss << _callstack[i].filename << ":" << _callstack[i].line << "\r\n";
+			if (_callstack[i].data == "") {
+				ss << _callstack[i].filename << ":" << _callstack[i].line << "\r\n";
+			}
+			else {
+				ss << _callstack[i].filename << ":" << _callstack[i].line << " {\r\n" << _callstack[i].data << "\r\n}\r\n";
+			}
 		}
 
 		return ss.str();
--- fxconf3/fxconf3/fxc/debug/Debug.h
+++ fxconf3/fxconf3/fxc/debug/Debug.h
@@ -12,12 +12,14 @@
 		struct SourcePoint {
 			const char *filename;
 			int line;
-			SourcePoint(const char *filename, int line);
+			const std::string data;
+
+			SourcePoint(const char *filename, int line, const std::string data);
 		};
 
 		class StackTrace {
 			public:
-				static void push(const char *filename, int line);
+				static void push(const char *filename, int line, std::string data = "");
 				static void pop();
 				static std::string trace();
 			private:
@@ -28,11 +30,13 @@
 
 	}
 
-	#define MARK_FUNC_IN fxc::debug::StackTrace::push(__FILE__, __LINE__);
-	#define MARK_FUNC_OUT fxc::debug::StackTrace::pop();
-	#define STACK_TRACE fxc::debug::StackTrace::trace()
+	#define MARK_FUNC_IN      fxc::debug::StackTrace::push(__FILE__, __LINE__);
+	#define MARK_FUNC_IN_T(t) fxc::debug::StackTrace::push(__FILE__, __LINE__, t);
+	#define MARK_FUNC_OUT     fxc::debug::StackTrace::pop();
+	#define STACK_TRACE       fxc::debug::StackTrace::trace()
 #else
 	#define MARK_FUNC_IN
+	#define MARK_FUNC_IN_T(t) 
 	#define MARK_FUNC_OUT
 	#define STACK_TRACE ""
 #endif
